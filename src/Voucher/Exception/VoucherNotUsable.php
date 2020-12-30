61506bc659a63eb9d1ece00e9a7ba1ad24b0733c
Anton Fedurtsya <anton@fedurtsya.com>
2020-12-30 12:09:30 +0200

    OXDEV-3954 Cleanup comments in VoucherNotUsable exception

--- src/Voucher/Exception/VoucherNotUsable.php
+++ src/Voucher/Exception/VoucherNotUsable.php
@@ -11,20 +11,6 @@ namespace OxidEsales\GraphQL\Storefront\Voucher\Exception;
 
 use OxidEsales\GraphQL\Base\Exception\Exists;
 
-// From core:
-//
-//'ERROR_MESSAGE_VOUCHER_INCORRECTPRICE' on too low price
-//'ERROR_MESSAGE_VOUCHER_NOTALLOWEDOTHERSERIES' on other series limitation
-//'ERROR_MESSAGE_VOUCHER_NOTALLOWEDSAMESERIES' on same series limitation
-//'ERROR_MESSAGE_VOUCHER_NOTVALIDUSERGROUP' on incorrect user group,
-//'MESSAGE_COUPON_EXPIRED' on expired coupon
-//'ERROR_MESSAGE_VOUCHER_NOVOUCHER' if coupon is not yet active
-//
-// Module special:
-//
-//'MESSAGE_COUPON_NOT_APPLIED_FOR_ARTICLES' if there are no products (specific product or category configuration does not fit) for this voucher
-//'MESSAGE_COUPON_NOT_APPLIED_FOR_SHOP' if coupon serie is not configured for current shop
-
 final class VoucherNotUsable extends Exists
 {
     public static function withMessage(string $message): self
