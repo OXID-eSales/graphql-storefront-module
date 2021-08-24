6ebdfc4d9dc98f390be92eec92e705aadf036e71
Anton Fedurtsya <anton@fedurtsya.com>
2021-08-24 13:11:57 +0300

    Add parent aliases in tests only during special sonarcloud related coverage run

--- tests/additional.inc.php
+++ tests/additional.inc.php
@@ -24,19 +24,21 @@ if ((new Facts())->isEnterprise()) {
     $serviceCaller->callService('ShopPreparation', 1);
 }
 
-class_alias(
-    \OxidEsales\Eshop\Application\Model\Basket::class,
-    \OxidEsales\GraphQL\Storefront\Shared\Shop\Basket_parent::class
-);
-class_alias(
-    OxidEsales\Eshop\Application\Model\User::class,
-    \OxidEsales\GraphQL\Storefront\Shared\Shop\User_parent::class
-);
-class_alias(
-    \OxidEsales\Eshop\Application\Model\Voucher::class,
-    \OxidEsales\GraphQL\Storefront\Shared\Shop\Voucher_parent::class
-);
-class_alias(
-    \OxidEsales\Eshop\Core\Language::class,
-    \OxidEsales\GraphQL\Storefront\Shared\Shop\Language_parent::class
-);
+if (getenv('STOREFRONT_COVERAGE')) {
+    class_alias(
+        \OxidEsales\Eshop\Application\Model\Basket::class,
+        \OxidEsales\GraphQL\Storefront\Shared\Shop\Basket_parent::class
+    );
+    class_alias(
+        OxidEsales\Eshop\Application\Model\User::class,
+        \OxidEsales\GraphQL\Storefront\Shared\Shop\User_parent::class
+    );
+    class_alias(
+        \OxidEsales\Eshop\Application\Model\Voucher::class,
+        \OxidEsales\GraphQL\Storefront\Shared\Shop\Voucher_parent::class
+    );
+    class_alias(
+        \OxidEsales\Eshop\Core\Language::class,
+        \OxidEsales\GraphQL\Storefront\Shared\Shop\Language_parent::class
+    );
+}
