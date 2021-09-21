5cc0c667cc93b29e5386ab623609454dc8e83a52
j.skoczek <j.skoczek@createit.pl>
2021-09-21 15:31:34 +0200

    remove code duplication intorduced with public basket

--- src/Basket/DataType/Basket.php
+++ src/Basket/DataType/Basket.php
@@ -9,10 +9,8 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Storefront\Basket\DataType;
 
-use DateTimeInterface;
 use OxidEsales\Eshop\Application\Model\UserBasket as BasketModel;
 use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
-use OxidEsales\GraphQL\Base\DataType\DateTimeImmutableFactory;
 use OxidEsales\GraphQL\Storefront\Basket\Event\BasketAuthorization;
 use OxidEsales\GraphQL\Storefront\Shared\DataType\DataType;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -23,7 +21,7 @@ use TheCodingMachine\GraphQLite\Types\ID;
 /**
  * @Type()
  */
-final class Basket implements DataType
+final class Basket extends AbstractBasket implements DataType
 {
     /** @var BasketModel */
     private $basket;
@@ -32,33 +30,7 @@ final class Basket implements DataType
         BasketModel $basket
     ) {
         $this->basket = $basket;
-    }
-
-    public function getEshopModel(): BasketModel
-    {
-        return $this->basket;
-    }
-
-    /**
-     * @Field()
-     */
-    public function id(): ID
-    {
-        return new ID(
-            $this->basket->getId()
-        );
-    }
-
-    /**
-     * @Field()
-     * Beware of the following values with special meaning
-     * - wishList
-     * - noticeList
-     * - savedBasket
-     */
-    public function title(): string
-    {
-        return (string) $this->basket->getFieldData('oxtitle');
+        parent::__construct($basket);
     }
 
     /**
@@ -67,35 +39,6 @@ final class Basket implements DataType
     public function public(): bool
     {
         return (bool) $this->basket->getFieldData('oxpublic');
-    }
-
-    /**
-     * @Field()
-     */
-    public function creationDate(): ?DateTimeInterface
-    {
-        return DateTimeImmutableFactory::fromString((string) $this->basket->getFieldData('oxtimestamp'));
-    }
-
-    /**
-     * @Field()
-     */
-    public function lastUpdateDate(): ?DateTimeInterface
-    {
-        $timeStamp = (int) $this->basket->getFieldData('oxupdate');
-
-        if ($timeStamp > 0) {
-            return DateTimeImmutableFactory::fromTimeStamp($timeStamp);
-        }
-
-        return null;
-    }
-
-    public function getUserId(): ID
-    {
-        return new ID(
-            (string) $this->basket->getFieldData('oxuserid')
-        );
     }
 
     public function getDeliveryAddressId(): ID
