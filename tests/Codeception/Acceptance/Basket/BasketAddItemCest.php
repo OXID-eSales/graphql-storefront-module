2286ab1a532c09e2cc577581828855ffe29f12d7
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-01-19 15:09:50 +0200

    OXDEV-4429 Add more out of stock tests

--- tests/Codeception/Acceptance/Basket/BasketAddItemCest.php
+++ tests/Codeception/Acceptance/Basket/BasketAddItemCest.php
@@ -9,8 +9,10 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Storefront\Tests\Codeception\Acceptance\Basket;
 
+use OxidEsales\GraphQL\Storefront\Basket\Exception\BasketAccessForbidden;
 use OxidEsales\GraphQL\Storefront\Basket\Exception\BasketItemAmountLimitedStock;
-use OxidEsales\GraphQL\Storefront\Tests\Codeception\Acceptance\BaseCest;
+use OxidEsales\GraphQL\Storefront\Basket\Exception\BasketNotFound;
+use OxidEsales\GraphQL\Storefront\Product\Exception\ProductNotFound;
 use OxidEsales\GraphQL\Storefront\Tests\Codeception\AcceptanceTester;
 use TheCodingMachine\GraphQLite\Middlewares\MissingAuthorizationException as MissingAuthorizationExceptionAlias;
 
@@ -18,7 +20,7 @@ use TheCodingMachine\GraphQLite\Middlewares\MissingAuthorizationException as Mis
  * @group basket
  * @group oe_graphql_storefront
  */
-final class BasketAddItemCest extends BaseCest
+final class BasketAddItemCest extends BasketBaseCest
 {
     // Public basket
     private const PUBLIC_BASKET = '_test_basket_public';
@@ -57,12 +59,9 @@ final class BasketAddItemCest extends BaseCest
 
     public function testAddItemToBasketNoToken(AcceptanceTester $I): void
     {
-        $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID);
+        $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID);
 
-        $I->seeResponseIsJson();
-        $result = $I->grabJsonResponseAsArray();
-
-        $I->assertStringStartsWith(
+        $I->assertSame(
             MissingAuthorizationExceptionAlias::forbidden()->getMessage(),
             $result['errors'][0]['message']
         );
@@ -87,13 +86,10 @@ final class BasketAddItemCest extends BaseCest
     {
         $I->login(self::USERNAME, self::PASSWORD);
 
-        $this->basketAddItemMutation($I, 'non_existing_basket_id', self::PRODUCT_ID);
-
-        $I->seeResponseIsJson();
-        $result = $I->grabJsonResponseAsArray();
+        $result = $this->basketAddItemMutation($I, 'non_existing_basket_id', self::PRODUCT_ID);
 
         $I->assertSame(
-            'Basket was not found by id: non_existing_basket_id',
+            BasketNotFound::byId('non_existing_basket_id')->getMessage(),
             $result['errors'][0]['message']
         );
     }
@@ -104,8 +100,8 @@ final class BasketAddItemCest extends BaseCest
 
         $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID, 2);
 
-        $basketData = $result['data']['basketAddItem'];
-        $I->assertSame(self::PUBLIC_BASKET, $basketData['id']);
+        $basket = $result['data']['basketAddItem'];
+        $I->assertSame(self::PUBLIC_BASKET, $basket['id']);
         $I->assertSame([
             [
                 'product' => [
@@ -118,8 +114,8 @@ final class BasketAddItemCest extends BaseCest
                 ],
                 'amount' => 1,
             ],
-        ], $basketData['items']);
-        $I->assertNotNull($basketData['lastUpdateDate']);
+        ], $basket['items']);
+        $I->assertNotNull($basket['lastUpdateDate']);
 
         $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID, 0);
     }
@@ -128,16 +124,7 @@ final class BasketAddItemCest extends BaseCest
     {
         $I->login(self::USERNAME, self::PASSWORD);
 
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 3,
-                'oxstock'     => 15,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, 15, 3);
 
         $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID, 200);
 
@@ -162,15 +149,7 @@ final class BasketAddItemCest extends BaseCest
         $I->assertNotNull($basketData['lastUpdateDate']);
 
         // reset article
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 1,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, 15, 1);
     }
 
     public function testAddItemToBasketWhenCurrentAmountIsBiggerThanStock(AcceptanceTester $I): void
@@ -194,18 +173,9 @@ final class BasketAddItemCest extends BaseCest
         ], $basketData['items']);
 
         // update stock flag to "If out of stock, not orderable"
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 3,
-                'oxstock'     => 15,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, 15, 3);
 
-        $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID, 1);
+        $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID);
 
         $basketData = $result['data']['basketAddItem'];
         $I->assertSame(self::PUBLIC_BASKET, $basketData['id']);
@@ -228,15 +198,7 @@ final class BasketAddItemCest extends BaseCest
         $I->assertNotNull($basketData['lastUpdateDate']);
 
         // reset article
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 1,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, 15, 1);
     }
 
     /** @group oe_graphql_storefront_basket_limited_stock */
@@ -247,26 +209,17 @@ final class BasketAddItemCest extends BaseCest
         $initialAmount = 3;
         $I->login(self::USERNAME, self::PASSWORD);
 
-        $this->basketId = $basketId = $this->basketCreateMutation($I);
-        $basketItem     = $this->basketAddItemMutation($I, $basketId, self::PRODUCT_ID, $initialAmount)['data']['basketAddItem']['items'][0];
+        $basketId   = $this->basketCreateMutation($I, self::BASKET_TITLE)['id'];
+        $basketItem = $this->basketAddItemMutation($I, $basketId, self::PRODUCT_ID, $initialAmount)['data']['basketAddItem']['items'][0];
         $I->assertEquals($initialAmount, $basketItem['amount']);
 
         //there's one more item in stock we can add to basket
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 3,
-                'oxstock'     => $initialAmount + 1,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, $initialAmount + 1, 3);
 
         //query the basket, up to now all is well
-        $result = $this->basketQuery($I, $basketId);
+        $basket = $this->basketQuery($I, $basketId);
         $I->assertFalse(isset($result['errors']));
-        $I->assertEquals($initialAmount, $result['data']['basket']['items'][0]['amount']);
+        $I->assertEquals($initialAmount, $basket['items'][0]['amount']);
 
         //try to add more of the main items, but only one more can be added
         $addAmount       = 10;
@@ -275,8 +228,8 @@ final class BasketAddItemCest extends BaseCest
         $I->assertStringStartsWith($expectedMessage, $result['errors'][0]['message']);
 
         //check the basket, we now should have $initialAmount + 1
-        $result = $this->basketQuery($I, $basketId);
-        $I->assertEquals($initialAmount + 1, $result['data']['basket']['items'][0]['amount']);
+        $basket = $this->basketQuery($I, $basketId);
+        $I->assertEquals($initialAmount + 1, $basket['items'][0]['amount']);
     }
 
     public function testAutomaticallyRemoveOutOfStockItemFromBasketOnAdd(AcceptanceTester $I): void
@@ -299,18 +252,9 @@ final class BasketAddItemCest extends BaseCest
             ],
         ], $basketData['items']);
 
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 3,
-                'oxstock'     => 0,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, 0, 3);
 
-        $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID, 1);
+        $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, self::PRODUCT_ID);
 
         $basketData = $result['data']['basketAddItem'];
         $I->assertSame(self::PUBLIC_BASKET, $basketData['id']);
@@ -326,29 +270,28 @@ final class BasketAddItemCest extends BaseCest
         $I->assertNotNull($basketData['lastUpdateDate']);
 
         // reset article
-        $I->updateInDatabase(
-            'oxarticles',
-            [
-                'oxstockflag' => 1,
-                'oxstock'     => 15,
-            ],
-            [
-                'oxid' => self::PRODUCT_ID,
-            ]
-        );
+        $this->updateStock($I, 15, 1);
+    }
+
+    public function addItemToBasketWithQuantityEqualToStock(AcceptanceTester $I): void
+    {
+        $this->updateStock($I, 2, 3);
+        $I->login(self::USERNAME, self::PASSWORD);
+
+        $basketId = $this->basketCreateMutation($I, 'test-stock-basket')['id'];
+        $result = $this->basketAddItemMutation($I, $basketId, self::PRODUCT_ID, 2);
+
+        $I->assertArrayNotHasKey('errors', $result);
+        $I->assertEquals(2, $result['data']['basketAddItem']['items'][0]['amount']);
     }
 
     public function testAddNonExistingItemToBasket(AcceptanceTester $I): void
     {
         $I->login(self::USERNAME, self::PASSWORD);
-
-        $this->basketAddItemMutation($I, self::PUBLIC_BASKET, 'non_existing_product');
-
-        $I->seeResponseIsJson();
-        $result = $I->grabJsonResponseAsArray();
+        $result = $this->basketAddItemMutation($I, self::PUBLIC_BASKET, 'non_existing_product');
 
         $I->assertSame(
-            'Product was not found by id: non_existing_product',
+            ProductNotFound::byId('non_existing_product')->getMessage(),
             $result['errors'][0]['message']
         );
     }
@@ -356,91 +299,25 @@ final class BasketAddItemCest extends BaseCest
     public function testAddItemToSomeoneElseBasket(AcceptanceTester $I): void
     {
         $I->login(self::USERNAME, self::PASSWORD);
-        $this->basketAddItemMutation($I, self::PRIVATE_BASKET, self::PRODUCT_FOR_PRIVATE_BASKET);
-
-        $I->seeResponseIsJson();
-        $result = $I->grabJsonResponseAsArray();
+        $result = $this->basketAddItemMutation($I, self::PRIVATE_BASKET, self::PRODUCT_FOR_PRIVATE_BASKET);
 
         $I->assertSame(
-            'You are not allowed to access this basket as it belongs to somebody else',
+            BasketAccessForbidden::byAuthenticatedUser()->getMessage(),
             $result['errors'][0]['message']
         );
     }
 
-    protected function basketCreateMutation(
-        AcceptanceTester $I,
-        string $title = self::BASKET_TITLE
-    ): string {
-        $mutation = '
-            mutation ($title: String!) {
-                basketCreate(basket: {title: $title}){
-                    id
-                }
-            }
-        ';
-
-        $variables = [
-            'title' => $title,
-        ];
-
-        $I->sendGQLQuery($mutation, $variables);
-        $I->seeResponseIsJson();
-
-        return $I->grabJsonResponseAsArray()['data']['basketCreate']['id'];
-    }
-
-    protected function basketQuery(
-        AcceptanceTester $I,
-        string $basketId
-    ): array {
-        $query = '
-            query ($basketId: ID!) {
-                basket(basketId: $basketId) {
-                    id
-                    items {
-                        amount
-                        id
-                        product {
-                            id
-                        }
-                    }
-                }
-            }
-        ';
-
-        $variables = [
-            'basketId' => $basketId,
-        ];
-
-        $I->sendGQLQuery($query, $variables);
-        $I->seeResponseIsJson();
-
-        return $I->grabJsonResponseAsArray();
-    }
-
-    private function basketAddItemMutation(AcceptanceTester $I, string $basketId, string $productId, int $amount = 1): array
+    private function updateStock(AcceptanceTester $I, int $stock, int $flag)
     {
-        $I->sendGQLQuery('
-            mutation {
-                basketAddItem(
-                    basketId: "' . $basketId . '",
-                    productId: "' . $productId . '",
-                    amount: ' . $amount . '
-                ) {
-                    id
-                    items {
-                        product {
-                            id
-                        }
-                        amount
-                    }
-                    lastUpdateDate
-                }
-            }
-        ');
-
-        $I->seeResponseIsJson();
-
-        return $I->grabJsonResponseAsArray();
+        $I->updateInDatabase(
+            'oxarticles',
+            [
+                'oxstockflag' => $flag,
+                'oxstock'     => $stock,
+            ],
+            [
+                'oxid' => self::PRODUCT_ID,
+            ]
+        );
     }
 }
