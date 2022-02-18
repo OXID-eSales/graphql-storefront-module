0dff8c5c268c2ef243e968e4432f7798738f6b6b
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-02-18 13:54:40 +0200

    Set empty session basket during initialization of session basket

--- src/Shared/Infrastructure/Basket.php
+++ src/Shared/Infrastructure/Basket.php
@@ -44,6 +44,7 @@ final class Basket
 
         /** @var StorefrontBasketModel $basketModel */
         $basketModel = oxNew(EshopBasketModel::class);
+        EshopRegistry::getSession()->setBasket($basketModel);
 
         foreach ($savedItems as $key => $savedItem) {
             $basketModel->addProductToBasket($savedItem);
