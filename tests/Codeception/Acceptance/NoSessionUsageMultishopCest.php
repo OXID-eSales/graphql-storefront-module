e22b9b729d8476dbbe5ef34b1cc9d47dc7028edf
RahatHameed <the_rahat_online@yahoo.com>
2024-05-17 12:16:07 +0200

    OXDEV-8324- Updated the groups effected by revert

--- tests/Codeception/Acceptance/NoSessionUsageMultishopCest.php
+++ tests/Codeception/Acceptance/NoSessionUsageMultishopCest.php
@@ -13,7 +13,7 @@ use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\GraphQL\Storefront\Tests\Codeception\AcceptanceTester;
 
 /**
- * @group memory_intense
+ * @group graphql_session_multishop
  * @group oe_graphql_storefront
  * @group other
  */
