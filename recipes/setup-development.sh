42aca00f0daac32952a312ac6dc3bb49231a6046
RahatHameed <therahatrajpoot@gmail.com>
2025-10-20 16:56:15 +0200

    OXDEV-8813 Compatibility with shop 7.4

--- recipes/setup-development.sh
+++ recipes/setup-development.sh
@@ -38,13 +38,13 @@ docker compose up --build -d php
 
 docker compose exec -T php git config --global --add safe.directory /var/www
 
-$SCRIPT_PATH/parts/shared/require_shop_edition_packages.sh -e"${edition}" -v"dev-b-7.3.x"
-$SCRIPT_PATH/parts/shared/require_twig_components.sh -e"${edition}" -b"b-7.3.x"
-$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/graphql-base" -g"https://github.com/OXID-eSales/graphql-base-module.git" -v"dev-b-7.3.x"
-$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/developer-tools" -v"dev-b-7.3.x"
-$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/oxideshop-doctrine-migration-wrapper" -v"dev-b-7.3.x"
+$SCRIPT_PATH/parts/shared/require_shop_edition_packages.sh -e"${edition}" -v"dev-b-7.4.x"
+$SCRIPT_PATH/parts/shared/require_twig_components.sh -e"${edition}" -b"b-7.4.x"
+$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/graphql-base" -g"https://github.com/OXID-eSales/graphql-base-module.git" -v"dev-b-7.4.x"
+$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/developer-tools" -v"dev-b-7.4.x"
+$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/oxideshop-doctrine-migration-wrapper" -v"dev-b-7.4.x"
 $SCRIPT_PATH/parts/shared/require_demodata_package.sh -e"${edition}" -b"master"
-$SCRIPT_PATH/parts/shared/require_theme.sh -t"apex" -b"b-7.3.x"
+$SCRIPT_PATH/parts/shared/require_theme.sh -t"apex" -b"b-7.4.x"
 
 docker compose exec -T php composer update --no-interaction
 
